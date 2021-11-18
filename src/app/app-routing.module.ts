76ec3253f442ade1219f1536c3c16f43d995979e
Hieu Ho <hieu_ho@helloleotech.com>
2021-11-18 22:23:59 +0700

    Initial project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./client/client.module').then(m => m.ClientModule)  },
+  { path: '', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/client/client-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ClientComponent } from './client.component';
+import { HomeComponent } from './main/home/home.component';
+
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./main/main.module').then(m => m.MainModule)  },
+  { path: '', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ClientRoutingModule { }
